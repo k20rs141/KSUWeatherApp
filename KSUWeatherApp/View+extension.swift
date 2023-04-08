080ca2f79001b38659c4fbb1ac08edeeeef77a0d
Yamada Kouki <90810018+k20rs141@users.noreply.github.com>
2023-04-08 23:57:18 +0900

    Create: modifierをExtension化

---
+++ KSUWeatherApp/View+extension.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension View {
+    func cardStyle(appearanceMode: ColorScheme, cornerRadius: CGFloat) -> some View {
+        self
+            .font(.system(size: 17))
+            .background(appearanceMode == .dark ? Color(red: 0.11, green: 0.12, blue: 0.27) : Color(red: 0.95, green: 0.95, blue: 0.95))
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
+    }
+}
